739182dbd1ea7b9d59ca7f443752deea240e572a
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-21 15:21:44 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import asahotak, { asahotakjson } from './games/asahotak'
-import family100, { family100json } from './games/family100'
+import asahotak, { asahotakjson } from "./games/asahotak"
+import family100, { family100json } from "./games/family100"
 
 export * from "./downloader/index"
 export { asahotak, family100 }
