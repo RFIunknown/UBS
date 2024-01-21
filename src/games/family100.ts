67c0d25a8f4335819884ac64c3e116be7f18823d
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-21 14:48:55 +0800

    Update family100.ts

--- src/games/family100.ts
+++ src/games/family100.ts
@@ -1,11 +1,11 @@
-import got from 'got'
-import { Family100, Family100Schema } from '../Types'
+import got from "got"
+import { Family100, Family100Schema } from "../Types"
 
 export let family100json: Family100[]
 export default async function family100 (): Promise<Family100> {
   if (!family100json) {
     family100json = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json'
+      "https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json"
     ).json()
   }
   return Family100Schema.parse(
