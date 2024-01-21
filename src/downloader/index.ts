a5c4191471c3a911473c6e52770a22beffa481d5
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-21 14:43:43 +0800

    Update index.ts

--- src/downloader/index.ts
+++ src/downloader/index.ts
@@ -1,7 +1,3 @@
 export * as youtube from "./youtube"
-export * as storyWa from "./statusWaIndonesia"
-export * from "./tiktok"
 export * from "./instagram"
-export * from "./zippyshare"
 export * from "./facebook"
-export * from "./pinterest"
