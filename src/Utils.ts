6cf2f0112b2c8596536aa70793d747296cd4c7a7
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-21 15:44:12 +0800

    Update Utils.ts

--- src/Utils.ts
+++ src/Utils.ts
@@ -17,3 +17,20 @@ export const Axios = CreateInstance()
 export function Cheerio (data: any): any {
 	return cheerio.load(data)
 }
+
+export function parseFileSize (size: string): number {
+  const sized = parseFloat(size)
+  return (isNaN(sized) ? 0 : sized) * (
+    /GB/i.test(size)
+      ? 1000000
+      : /MB/i.test(size)
+        ? 1000
+        : /KB/i.test(size)
+          ? 1
+          : /bytes?/i.test(size)
+            ? 0.001
+            : /B/i.test(size)
+              ? 0.1
+              : 0
+  )
+}
