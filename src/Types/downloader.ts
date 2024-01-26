228f729cf3481b7bd1ba5755e761fd342c4d39f3
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-26 19:00:01 +0800

    Update downloader.ts

--- src/Types/downloader.ts
+++ src/Types/downloader.ts
@@ -84,3 +84,15 @@ export type FacebookDownloadResult = {
 	isHdAvailable: boolean;
 	urls: { sd?: string; hd?: string }[];
 };
+
+export type TiktokDownloadResult = {
+	unique_id: string;
+	nickname: string;
+	download_count: number;
+	duration: number;
+	description: string;
+	play: string;
+	wmplay: string;
+	hdplay: string;
+	music: string;
+};
