7034b9bebe290e354966c8d263b51b3f3485ea28
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-26 19:07:33 +0800

    Update downloader.ts

--- src/Types/downloader.ts
+++ src/Types/downloader.ts
@@ -86,13 +86,38 @@ export type FacebookDownloadResult = {
 };
 
 export type TiktokDownloadResult = {
-	unique_id: string;
-	nickname: string;
-	download_count: number;
-	duration: number;
-	description: string;
-	play: string;
-	wmplay: string;
-	hdplay: string;
-	music: string;
+  id: string;
+  region: string;
+  title: string;
+  duration: number;
+  play: string;
+  wmplay: string;
+  hdplay: string;
+  size: number;
+  wm_size: number;
+  hd_size: number;
+  music: string;
+  music_info: {
+    id: string;
+    title: string;
+    play: string;
+    author: string;
+    original: boolean;
+    duration: number;
+    album: string;
+  };
+  play_count: number;
+  digg_count: number;
+  comment_count: number;
+  share_count: number;
+  download_count: number;
+  collect_count: number;
+  create_time: number;
+  author: {
+    id: string;
+    unique_id: string;
+    nickname: string;
+    avatar: string;
+  };
 };
+
