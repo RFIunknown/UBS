0c5a68eeb4c7e7372af0f14c073137e18deaacd4
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-26 19:14:44 +0800

    Create tiktok.ts

---
+++ src/downloader/tiktok.ts
@@ -0,0 +1,121 @@
+import axios from 'axios';
+import { TikWM } from '../Constant';
+import { TiktokDownloadResult } from '../Types';
+import { errorHandling } from "../Interface"
+
+export interface TiktokScrapeResponse {
+  code: number;
+  msg: string;
+  processed_time: number;
+  data: {
+    id: string;
+    region: string;
+    title: string;
+    cover: string;
+    duration: number;
+    play: string;
+    wmplay: string;
+    hdplay: string;
+    size: number;
+    wm_size: number;
+    hd_size: number;
+    music: string;
+    music_info: {
+      id: string;
+      title: string;
+      play: string;
+      author: string;
+      original: boolean;
+      duration: number;
+      album: string;
+    };
+    play_count: number;
+    digg_count: number;
+    comment_count: number;
+    share_count: number;
+    download_count: number;
+    collect_count: number;
+    create_time: number;
+    author: {
+      id: string;
+      unique_id: string;
+      nickname: string;
+      avatar: string;
+    };
+  };
+}
+
+export async function tiktokScrape(url: string): Promise<TiktokDownloadResult | errorHandling> {
+  try {
+    const scrapeResult = await axios.post<TiktokScrapeResponse>(
+      TikWM,
+      { url, count: 12, cursor: 0, web: 1, hd: 1 }
+    );
+
+    if (scrapeResult.status === 200) {
+      const data = scrapeResult.data;
+
+      if (data && typeof data === 'object') {
+        if (data.code === 0 && data.data) {
+          const {
+            id,
+            region,
+            title,
+            duration,
+            play,
+            wmplay,
+            hdplay,
+            size,
+            wm_size,
+            hd_size,
+            music,
+            music_info,
+            play_count,
+            digg_count,
+            comment_count,
+            share_count,
+            download_count,
+            collect_count,
+            create_time,
+            author,
+          } = data.data;
+
+          return {
+            id,
+            region,
+            title,
+            duration,
+            play,
+            wmplay,
+            hdplay,
+            size,
+            wm_size,
+            hd_size,
+            music,
+            music_info,
+            play_count,
+            digg_count,
+            comment_count,
+            share_count,
+            download_count,
+            collect_count,
+            create_time,
+            author,
+          };
+        } else {
+          throw new Error(data.msg || `Failed to scrape data from ${TikWM}`);
+        }
+      } else {
+        throw new Error(data.msg || `Failed to scrape data from ${TikWM}`);
+      }
+    } else {
+      throw new Error(`Unexpected status code: ${scrapeResult.status}`);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    return {
+      error: true,
+      message: String(error),
+    };
+  }
+}
