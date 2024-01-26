94e1044189a66bf478201c98f2087e53bfb80138
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2024-01-26 19:16:42 +0800

    Update tiktok.ts

--- src/downloader/tiktok.ts
+++ src/downloader/tiktok.ts
@@ -45,7 +45,7 @@ export interface TiktokScrapeResponse {
   };
 }
 
-export async function tiktokScrape(url: string): Promise<TiktokDownloadResult | errorHandling> {
+export async function tiktokdl(url: string): Promise<TiktokDownloadResult | errorHandling> {
   try {
     const scrapeResult = await axios.post<TiktokScrapeResponse>(
       TikWM,
